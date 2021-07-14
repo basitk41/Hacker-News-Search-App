2c641a2a44441d909fd437e2fbad871f5475e018
Basit Ali <basitk41@gmail.com>
2021-07-14 19:23:27 +0500

    updated

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-com";
 import store from "./store/store";
 import { Provider } from "react-redux";
 
